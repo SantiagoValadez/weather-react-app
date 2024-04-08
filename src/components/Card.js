31470a4757e5eb24422984fe34da1bcc8c68c338
saniagoValadez <saniago1340@gmail.com>
2024-04-08 01:53:43 -0600

    se agrego mas informacion meteorológica

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,9 +9,17 @@ const Card = ({loadingData, showData, weather, forecast}) =>{
     let year = today.getFullYear();
     let date = day + '/' + month + '/' + year;
 
+    let url = '';
+    let iconUrl = '';
+
     if (loadingData){
         return <Spinner />
     }
+    if (showData){
+        url = 'http://openweathermap.org/img/w/';
+        iconUrl = url + weather.weather[0].icon + '.png'
+    }
+
     return (
         <div className="mt-5">
 
@@ -25,13 +33,20 @@ const Card = ({loadingData, showData, weather, forecast}) =>{
                                     <h3 className="card-title">{weather.name}</h3>
                                     <p className="card-date">{date}</p>
                                     <h1 className="card-temp">{(weather.main.temp - 273.15).toFixed(1)}°C</h1>
+                                    <p className="card-desc"><img src={iconUrl} alt="icon" />{weather.weather[0].description}</p>
 
                                     <img src="https://images.unsplash.com/photo-1512864733469-8c0d7cc3ccf5?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="img-fluid rounded-start" alt="..."/>
 
                                 </div>
                                 <div className="col-md-8">
                                     <div className="card-body text-start mt-2">
+                                        <h5 className="card-text">Temperatura máxima: {(weather.main.temp_max - 273.15).toFixed(1)}°C</h5>
+                                        <h5 className="card-text">Temperatura mínima: {(weather.main.temp_min - 273.15).toFixed(1)}°C</h5>
+                                        <h5 className="card-text">Sensación térmica: {(weather.main.feels_like - 273.15).toFixed(1)}°C</h5>
+                                        <h5 className="card-text">Humedad: {weather.main.humidity}%</h5>
+                                        <h5 className="card-text">Velocidad del viento: {weather.wind.speed}m/s</h5>
                                     </div>
+                                    <hr/>
                                 </div>
                             </div>
                         </div>
